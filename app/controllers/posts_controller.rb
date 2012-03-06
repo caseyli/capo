43495271afcb119bebdb1c59e68992d68ba9632a
Casey Li <casey.li@gmail.com>
2012-03-05 21:04:53 -0500

    Refactored Posts Controller and Views to take advantage of it being a nested resource.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,33 +1,33 @@
 class PostsController < ApplicationController
   
+  before_filter :load_open_mic
+  before_filter :except => [:index] do |controller|
+    deny_access_for_non_hosts(@open_mic)
+  end
+  
   def create
-    @post  = current_user.posts.build(params[:post])
-    @post.open_mic = OpenMic.find_by_id(params[:post][:open_mic_id])
-    
-    deny_access_for_non_hosts(@post.open_mic)
-    
+    @post  = @open_mic.posts.build(params[:post])
+    @post.user = current_user    
+        
     if @post.save
       flash[:success] = "Posted!"
     end
 
-    redirect_to @post.open_mic
+    redirect_to @open_mic
   end
 
-  #
-  # Called by /open_mics/:id/posts
-  #
   def index
-    @open_mic = OpenMic.find_by_id(params[:open_mic_id])
   end
   
   def destroy
-    @post = Post.find(params[:id])
-    @open_mic = @post.open_mic
-    
-    deny_access_for_non_hosts(@open_mic)
-    
+    @post = Post.find(params[:id])    
     @post.destroy
     redirect_to open_mic_posts_url(@open_mic)
   end
-
+  
+  private
+    def load_open_mic
+      @open_mic = OpenMic.find(params[:open_mic_id])
+    end
+    
 end
